c1f8f58f33f40e3c57d9bc1d70e3dd7eb4ed68aa
Wouter Wietses <wouter.wietses@ns.nl>
2024-10-16 10:06:49 +0200

    Restructure the package

--- TicTacToe/Package.swift
+++ TicTacToe/Package.swift
@@ -1,24 +1,22 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TicTacToe",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TicTacToe",
             targets: ["TicTacToe"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TicTacToe"),
+            name: "TicTacToe",
+            path: "Sources"),
         .testTarget(
-            name: "TicTacToeTests",
-            dependencies: ["TicTacToe"]
+            name: "Tests",
+            dependencies: ["TicTacToe"],
+            path: "Tests"
         ),
     ]
 )
