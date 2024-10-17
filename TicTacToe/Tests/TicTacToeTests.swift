5b874cc7e2e07be55f7c261beedb96f4d633ae17
Wouter Wietses <wouter.wietses@ns.nl>
2024-10-17 09:32:15 +0200

    [refactor] removed obsolete test function

--- TicTacToe/Tests/TicTacToeTests.swift
+++ TicTacToe/Tests/TicTacToeTests.swift
@@ -2,12 +2,6 @@ import Testing
 
 @testable import TicTacToe
 
-@Test func shouldHaveABoard() async throws {
-    let game = Game()
-
-    #expect(game.board != nil)
-}
-
 @Test func allPositionsAreEmptyInitially() async throws {
     let game = Game()
     
