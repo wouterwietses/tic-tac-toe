aec7e29cf3f0f446ce67b48b58a6864cc7678159
Wouter Wietses <wouter.wietses@ns.nl>
2024-10-17 11:29:19 +0200

    [green] set initial game state to in progress

--- TicTacToe/Sources/Game.swift
+++ TicTacToe/Sources/Game.swift
@@ -1,11 +1,12 @@
 final class Game {
 
     enum Status: Equatable {
+        case inProgress
         case playerWon(Player)
     }
 
     let board: Board = Board()
-    private(set) var status: Status = .playerWon(Player(marker: "X"))
+    private(set) var status: Status = .inProgress
 
     private let players: [Player] = [Player(marker: "X"), Player(marker: "O")]
 
@@ -18,6 +19,7 @@ final class Game {
     func play(position: Int) {
         board.play(position: position, marker: currentPlayer.marker)
         switchPlayers()
+        status = .playerWon(Player(marker: "X"))
     }
 
     private func switchPlayers() {
