868e51976d8d9066918f2db8f9cfd32a42b5ca4a
Wouter Wietses <wouter.wietses@ns.nl>
2024-10-17 09:15:06 +0200

    [refactor] removed obsolute test function

--- TicTacToe/Tests/TicTacToeTests.swift
+++ TicTacToe/Tests/TicTacToeTests.swift
@@ -2,10 +2,6 @@ import Testing
 
 @testable import TicTacToe
 
-@Test func shouldHaveAGame() async throws {
-    let game = Game()
-}
-
 @Test func shouldHaveABoard() async throws {
     let game = Game()
 
